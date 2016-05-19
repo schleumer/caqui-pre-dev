cfaf92f42f2019db69b82cd0428b52ae3b627d6d
Wesley Schleumer <wesley.schleumer@gmail.com>
2016-05-18 21:03:47 -0300

    -

---
+++ src/javascript/reducers/loading.jsx
@@ -0,0 +1,25 @@
+import * as constants from '../constants';
+
+const initialState = {
+    state: false,
+    message: null,
+    subMessage: null
+};
+
+export default function (state = initialState, action = {}) {
+    switch (action.type) {
+        case constants.IS_LOADING:
+            return {
+                ...state,
+                ...{
+                    state: false,
+                    message: null,
+                    subMessage: null
+                },
+                ...action.data
+            };
+
+        default:
+            return state;
+    }
+}
